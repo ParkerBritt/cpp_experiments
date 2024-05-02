abfb11e28536b8949e3a1a3cedc53df061232679
parker <parker.g.britt@gmail.com>
2024-05-03 00:48:07 +0100

    quantize pixels

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -1,24 +1,30 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <chrono>
 
 int main()
 {
-    int x_width = 800;
-    int y_width = 800;
-    sf::RenderWindow window(sf::VideoMode(x_width, y_width), "Testing Window");
+    int window_width = 800;
+    int window_height = 800;
+    int pixel_scale = 100;
+    int virtual_width = std::floor(float(window_width)/pixel_scale)*pixel_scale;
+    int virtual_height = std::floor(float(window_height)/pixel_scale)*pixel_scale;
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Testing Window");
     sf::CircleShape my_shape(100.f);
     my_shape.setFillColor(sf::Color::Blue);
     sf::Image my_image;
     sf::Sprite my_sprite;
-    my_image.create(x_width,y_width, sf::Color::Blue);
-    for(int x=0; x<x_width; x++){
-        for(int y=0; y<y_width; y++){
-            int brightness = float(x)/x_width*255;
-            std::cout << "brightness: " << brightness << std::endl;
+    my_image.create(window_width,window_height, sf::Color::Blue);
+    for(int x=0; x<window_width; x++){
+        int virtual_x = std::floor(float(x)/pixel_scale)*pixel_scale;
+        for(int y=0; y<window_height; y++){
+            int virtual_y = std::floor(float(y)/pixel_scale)*pixel_scale;
+            int brightness = (float(virtual_x+virtual_y)/(virtual_width+virtual_height))*255;
             my_image.setPixel(x, y, sf::Color(brightness,brightness,brightness,255));
         }
     }
+    // std::cout << "virtual_width: " << virtual_width << std::endl;
     sf::Texture my_texture;
     my_texture.loadFromImage(my_image);
     my_sprite.setTexture(my_texture);
@@ -29,6 +35,7 @@ int main()
 
     while (window.isOpen())
     {
+        auto timer_start = std::chrono::high_resolution_clock::now();
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -39,10 +46,14 @@ int main()
         window.clear();
         float rad = (std::sin(time)+1)/2*scale;
         my_shape.setRadius(rad);
-        // window.draw(my_shape);
-        window.draw(my_sprite);
+        window.draw(my_shape);
+        // window.draw(my_sprite);
         window.display();
         time+= speed;
+
+        auto timer_end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> duration = timer_end - timer_start; 
+        std::cout << duration.count() << " milliseconds." << std::endl;
     }
 
     return 0;
