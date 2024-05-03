de0b0637f187ef48503fab88109d0cd40fdbfdfe
parker <parker.g.britt@gmail.com>
2024-05-03 13:29:13 +0100

    class based cell grid

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -3,31 +3,44 @@
 #include <iostream>
 #include <chrono>
 
+class cell_grid : public sf::Drawable, public sf::Transformable{
+    public:
+        cell_grid(const int window_width, const int window_height){
+            int pixel_width = 100;
+            int virt_width = window_width/pixel_width;
+            int virt_height = window_height/pixel_width;
+            m_vertices.setPrimitiveType(sf::Quads);
+
+            for(int x=0; x<virt_width; x++){
+                for(int y=0; y<virt_height; y++){
+                    float brightness = float(x+y)/(virt_width+virt_height)*255;
+                    // brightness = 255;
+                    std::cout << brightness << std::endl;
+                    sf::Color color(brightness, brightness, brightness, 255);
+
+                    int virt_x = x*pixel_width;
+                    int virt_y = y*pixel_width;
+                    m_vertices.append(sf::Vertex(sf::Vector2f(  virt_x,   virt_y), color));
+                    m_vertices.append(sf::Vertex(sf::Vector2f(  virt_x, virt_y+pixel_width), color));
+                    m_vertices.append(sf::Vertex(sf::Vector2f( virt_x+pixel_width, virt_y+pixel_width), color));
+                    m_vertices.append(sf::Vertex(sf::Vector2f( virt_x+pixel_width, virt_y), color));
+                }
+            }
+        }
+    private:
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+        {
+            target.draw(m_vertices, states);
+        }
+        sf::VertexArray m_vertices;
+};
 int main()
 {
 
-    sf::VertexArray m_vertices(sf::Quads);
-    int window_width = 800;
-    int window_height = 800;
+    const int window_width = 800;
+    const int window_height = 800;
+    cell_grid foo(window_height, window_width);
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Test Window");
-    int pixel_width = 100;
-    int virt_width = window_width/pixel_width;
-    int virt_height = window_height/pixel_width;
-
-    for(int x=0; x<virt_width; x++){
-        for(int y=0; y<virt_height; y++){
-            float brightness = float(x+y)/(virt_width+virt_height)*255;
-            std::cout << brightness << std::endl;
-            sf::Color color(brightness, brightness, brightness, 255);
-
-            int virt_x = x*pixel_width;
-            int virt_y = y*pixel_width;
-            m_vertices.append(sf::Vertex(sf::Vector2f(  virt_x,   virt_y), color));
-            m_vertices.append(sf::Vertex(sf::Vector2f(  virt_x, virt_y+pixel_width), color));
-            m_vertices.append(sf::Vertex(sf::Vector2f( virt_x+pixel_width, virt_y+pixel_width), color));
-            m_vertices.append(sf::Vertex(sf::Vector2f( virt_x+pixel_width, virt_y), color));
-        }
-    }
  
     float i = 0;
     while (window.isOpen())
@@ -47,7 +60,8 @@ int main()
         //     m_vertices[i].color = sf::Color(brightness, brightness, brightness, 255);
         //     
         // }
-        window.draw(m_vertices);
+        // window.draw(m_vertices);
+        window.draw(foo);
         window.display();
 
         auto end = std::chrono::high_resolution_clock::now();
