d61067b1bae82ce33860b6caceb14407b38d9fa8
parker <parker.g.britt@gmail.com>
2024-05-05 02:30:48 +0100

    add deltatime; fix vert buildup bug; optimize color setting

--- cellular_automata/main.cpp
+++ cellular_automata/main.cpp
@@ -6,18 +6,19 @@
 #include <vector>
 
 const std::string WINDOW_TITLE = "";
-class cell_grid : public sf::Drawable, public sf::Transformable{
+class CellGrid : public sf::Drawable, public sf::Transformable{
     public:
         int virt_height;
         int virt_width;
         sf::VertexArray m_vertices;
-        std::vector<std::vector<float>> grid_array;
-        int pixel_width = 100;
-        cell_grid(const int window_width, const int window_height){
+        std::vector<std::vector<int>> grid_array;
+        std::vector<std::vector<int>> prev_grid_array;
+        int pixel_width = 1;
+        CellGrid(const int window_width, const int window_height){
             virt_width = window_width/pixel_width;
             virt_height = window_height/pixel_width;
             m_vertices.setPrimitiveType(sf::Quads);
-            grid_array.resize(virt_width, std::vector<float>(virt_height));
+            grid_array.resize(virt_width, std::vector<int>(virt_height));
 
 
             for(int y=0; y<virt_height; y++){
@@ -26,10 +27,9 @@ class cell_grid : public sf::Drawable, public sf::Transformable{
                     grid_array[x][y] = brightness;
                 }
             }
-            refresh_verts();
+            prev_grid_array = grid_array;
 
-        }
-        void refresh_verts(){
+            // set initial vert color
             for(int y=0; y<virt_height; y++){
                 for(int x=0; x<virt_width; x++){
                     float brightness = grid_array[x][y];
@@ -43,13 +43,32 @@ class cell_grid : public sf::Drawable, public sf::Transformable{
                     m_vertices.append(sf::Vertex(sf::Vector2f( virt_x+pixel_width, virt_y), color));
                 }
             }
+
         }
+        void refresh_verts(){
+            for(int y=0; y<virt_height; y++){
+                for(int x=0; x<virt_width; x++){
+                    if(prev_grid_array[x][y]==grid_array[x][y]){
+                        continue;
+                    }
+                    int cell_value = grid_array[x][y];
+                    sf::Color color(cell_value, cell_value, cell_value, 255);
+
+                    set_vert_color(x, y, color);
+                }
+            }
+            prev_grid_array = grid_array;
+        }
+
         sf::Color get_color(const int x, const int y){
             int index = x*4+(y*virt_height*4);
                 return m_vertices[index].color;
 
         }
-        void set_color(const int x, const int y, sf::Color color){
+        void set_color(const int x, const int y, int cell_value){
+            grid_array[x][y] = cell_value;
+        }
+        void set_vert_color(const int x, const int y, sf::Color color){
             int index = x*4+(y*virt_height*4);
             for(int i=0; i<4; i++){
                 m_vertices[index+i].color = color;
@@ -67,25 +86,26 @@ int main()
 
     const int window_width = 800;
     const int window_height = 800;
-    cell_grid foo(window_height, window_width);
+    CellGrid foo(window_height, window_width);
 
-    std::vector<std::vector<float>> grid_array = foo.grid_array;
+    std::vector<std::vector<int>> grid_array_default_state = foo.grid_array;
     foo.refresh_verts();
-    for(int x = 0; x<foo.grid_array.size(); x++){
-        for(int y=0; y<foo.grid_array[0].size(); y++){
-            std::cout << foo.grid_array[x][y] << " ";
-        } 
-        std::cout << std::endl;
-    }
-    // return 0;
+
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), WINDOW_TITLE);
  
     float i = 0;
     int selected_x = 0;
     int selected_y = 0;
+
+
+    sf::Clock deltaClock;
+    sf::Time updateInterval = sf::seconds(0.01);
+    sf::Time elapsedTimeSinceLastUpdate = sf::Time::Zero;
+
     while (window.isOpen())
     {
         auto start = std::chrono::high_resolution_clock::now();
+        sf::Time dt = deltaClock.restart();
         // handle events
         sf::Event event;
         while (window.pollEvent(event))
@@ -93,28 +113,38 @@ int main()
             if(event.type == sf::Event::Closed)
                 window.close();
         }
-        for(int x = 0; x<foo.grid_array.size(); x++){
-            for(int y=0; y<foo.grid_array[0].size(); y++){
-                foo.grid_array[x][y] = grid_array[x][y];
-            } 
-        }
-        foo.grid_array[selected_x][selected_y] = 255;
-        foo.refresh_verts();
 
-        window.clear();
-        window.draw(foo);
-        window.display();
+        elapsedTimeSinceLastUpdate += dt;
+        if(elapsedTimeSinceLastUpdate >= updateInterval || true){
+            // reset set grid colors to initial color
+            for(int x = 0; x<foo.grid_array.size(); x++){
+                for(int y=0; y<foo.grid_array[0].size(); y++){
+                    foo.grid_array[x][y] = grid_array_default_state[x][y];
+                } 
+            }
+            // set changing cell to full bright
+            foo.set_color(selected_x, selected_y, 255);
+            foo.refresh_verts();
 
-        selected_x++;
-        if(selected_x > foo.virt_width-1){
-            selected_x = 0;
-            selected_y = (selected_y + 1)%(foo.virt_height);
-            std::cout << "COORDS" << selected_x << " " << selected_y << std::endl;
+            // draw frame
+            window.clear();
+            window.draw(foo);
+            window.display();
+
+            // iterate
+            selected_x++;
+            if(selected_x > foo.virt_width-1){
+                selected_x = 0;
+                selected_y = (selected_y + 1)%(foo.virt_height);
+            }
+            elapsedTimeSinceLastUpdate = sf::Time::Zero;
         }
+
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double, std::milli> duration = end - start;
-        std::cout << duration.count() << " milliseconds." << std::endl;
-        usleep(100000);
+        // std::cout << duration.count() << " milliseconds" << std::endl;
+        std::cout << dt.asMilliseconds() << " milliseconds" << std::endl;
+        // usleep(100000);
     }
 
     return 0;
