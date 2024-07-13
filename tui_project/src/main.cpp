4b89667dff7dafceedc0ec5a5b3336cce70c6a89
parker <parker.g.britt@gmail.com>
2024-07-13 17:10:15 +0100

    split XDG_DATA_DIRS into unique string vector

--- tui_project/src/main.cpp
+++ tui_project/src/main.cpp
@@ -1,12 +1,16 @@
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
 
 #include <cstdlib>
+#include <filesystem>
+#include <stdexcept>
 
 namespace ui = ftxui;
 int main(){
-     auto screen = ftxui::ScreenInteractive::TerminalOutput();
+    auto screen = ftxui::ScreenInteractive::TerminalOutput();
 
     std::string inputStr = "";
     ftxui::InputOption inputOptions = ui::InputOption::Spacious();
@@ -24,11 +28,26 @@ int main(){
     auto renderer = ui::Renderer(mainLayout, [&] {
         return ui::vbox({
             input->Render(),
-            ui::text("world" + inputStr),
+            ui::text("input string: \"" + inputStr + "\""),
         }) | ui::borderRounded;
     });
-    if(const char* dataDirs = std::getenv("XDG_DATA_DIRS")){
-        std::cout << "val: " << std::getenv("XDG_DATA_DIRS") << std::endl;;
+    const char* envTemp = std::getenv("XDG_DATA_DIRS");
+    if(envTemp){
+        // std::cout << "val: " << std::getenv("XDG_DATA_DIRS") << std::endl;;
+        std::string dataDirsRaw = envTemp;
+        std::vector<std::string> dataDirsSplit;
+        boost::split(dataDirsSplit, dataDirsRaw, boost::is_any_of(":"));
+
+        std::sort(dataDirsSplit.begin(), dataDirsSplit.end());
+        std::vector<std::string>::iterator lastUniquePos = std::unique(dataDirsSplit.begin(), dataDirsSplit.end());
+        dataDirsSplit.erase(lastUniquePos, dataDirsSplit.end());
+
+        for(int i=0; i<dataDirsSplit.size(); i++){
+            std::cout << "path: " << dataDirsSplit[i] << std::endl;
+        }
+    }
+    else{
+        throw std::runtime_error("XDG_DATA_DIRS not set");
     }
 
     screen.Loop(renderer);
