16ecc0ff44f021d7bca48e7532e296fbd27066b2
parker <parker.g.britt@gmail.com>
2024-07-17 18:49:36 +0100

    add search bar class

---
+++ tui_application_launcher/include/CompSearchBar.cpp
@@ -0,0 +1,18 @@
+#ifndef COMP_SEARCH_BAR_HPP
+#define COMP_SEARCH_BAR_HPP
+
+#include <ftxui/component/component.hpp>
+
+namespace ui = ftxui;
+
+class SearchBar : public ui::ComponentBase {
+public:
+
+
+private:
+    ui::Component input_;
+
+};
+
+
+#endif
