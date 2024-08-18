376f0aafd79df12394705dedbd0badc5266b4d24
parker <parker@parkerbritt.com>
2024-08-17 23:40:40 -0700

    fix iconmap import

--- tui_application_launcher/include/IconMap.hpp
+++ tui_application_launcher/include/IconMap.hpp
@@ -4,42 +4,6 @@
 #include <unordered_map>
 #include <string>
 
-std::unordered_map<std::string, std::string> iconMap = {
-    {"discord", "󰙯"},
-    {"firefox", "󰈹"},
-    {"spotify", ""},
-    {"gnu image manipulation program", ""},
-    {"customize look and feel", "󰔎"},
-    {"pulseaudio volume control", ""},
-    {"gparted", ""},
-    {"fish", ""},
-    {"signal", ""},
-    {"manage printing", "󰐪"},
-
-    {"btop", ""},
-    {"btop++", ""},
-    {"rofi", ""},
-
-    {"bluetooth manager", ""},
-    {"bluetooth adapters", ""},
-
-    {"alacritty", ""},
-    {"kitty", ""},
-    {"konsole", ""},
-
-    {"qbittorrent", ""},
-    {"proton vpn", ""},
-    {"advanced network configuration", ""},
-
-    {"neovim wrapper", "󰂮"},
-    {"lunarvim", "󰂮"},
-    {"zed", "󰂮"},
-    {"kwrite", "󰂮"},
-    {"kate", "󰂮"},
-
-    {"a widget factory", "󰊬"},
-    {"gtk settings", "󰊬"},
-
-};
+extern std::unordered_map<std::string, std::string> iconMap;
 
 #endif
